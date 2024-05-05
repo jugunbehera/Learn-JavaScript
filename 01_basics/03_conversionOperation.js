383dce56ca95923ac3d05a7ccd028ecacff9eb02
jugunbehera <117174558+jugunbehera@users.noreply.github.com>
2024-05-05 08:28:31 +0000

    conversion operation check in two

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,34 @@
-let UserValue = 33
+// let UserValue = undefined
 
-let NumberUserValue = Number(UserValue)
+// let NumberUserValue = Number(UserValue)
 
-console.log(UserValue)
-// console.log(NumberUserValue)
-// console.log(typeof NumberUserValue)
+//  console.log(UserValue)
+//  console.log(typeof UserValue)
+//  console.log(NumberUserValue)
+//  console.log(typeof NumberUserValue)
+
+ let isLoggedIn = "Jugun"
+ let isLoggedInBoolean = Boolean(isLoggedIn)
+ 
+ console.log(isLoggedIn)
+ console.log(typeof isLoggedIn)
+ console.log(isLoggedInBoolean)
+ console.log(typeof isLoggedInBoolean)
+ 
+
+ /*
+    Number function retuns (type):
+    33 or "33" =  returns 33 & Type as number
+    33abc = returns NaN (not a number) & type as number (weired)
+    true/false = returns 1 or 0 & type as Number
+    null = retuens 0 & type as Number
+    undefined = returns NaN & type as number 
+    -----------------------------------------------
+    Boolean function returns (type):
+    1 = returns true 
+    0 = returns false
+    "" = returns false
+    "Jugun" = returns true
+*/
+//node 01_basics/03_conversionOperation.js
+
