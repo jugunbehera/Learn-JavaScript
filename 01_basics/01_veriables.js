f5969a265fe765381002d148a955424a2ea357a5
jugunbehera <117174558+jugunbehera@users.noreply.github.com>
2024-05-05 06:29:46 +0000

    Check-in for veriables tutorial

---
+++ 01_basics/01_veriables.js
@@ -0,0 +1,15 @@
+//How to declare veriables
+//04 ways to create veriables
+
+const CompanyName = "Odessa Technology"
+var CompanyEmail = "OdessaCompany@gmail.com"
+let CompanyPassword = "787823445"
+CompanyCity = "Bengaluru"
+let CompanyState
+
+console.log(CompanyName);
+console.table([CompanyName,CompanyEmail,CompanyPassword,CompanyCity,CompanyState])
+
+/*
+    Prefer not to use var, because of issue of block scope & functional scope.
+*/
