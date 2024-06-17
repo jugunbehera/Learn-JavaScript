60a43deb6f119156eb2300fa99b4c0567018b5f1
jugunbehera <117174558+jugunbehera@users.noreply.github.com>
2024-06-17 02:34:55 +0000

    DataType & Memory check in

---
+++ 01_basics/04_DTnMemory.js
@@ -0,0 +1,31 @@
+/*
+    DataTypes are 2 Types
+    1. Primitive DataType
+        String,Number,Boolean,Null,Undefined,Symbol
+    2. Non-Primitive Data Type (Reference Type)
+        Array, Objects, Functions
+
+    Note : JavaScript is a Dynamically Typed Language. i.e. The Data Type of the
+           veriables are determined by the value they hold during run time. And can 
+           Change through out the programm as we assign different values to them.    
+*/
+
+//Array
+let Heroes = ['Shaktiman','Aryaman','Krrish','Junior-G']
+// console.log(Heroes[0]+' '+ Heroes[3]);
+
+//Object
+// let MyObject = {
+//     Name : 'Jugun Behera',
+//     Mobile : '9036507261',
+// }
+// console.log(MyObject.Name+' '+MyObject.Mobile);
+
+//Function
+// const MyFunction = Function(
+//     console.log("This is my Function!!!")
+// )
+
+//console.log(typeof Heroes);
+
+//typeof is a function used to find the data type.
