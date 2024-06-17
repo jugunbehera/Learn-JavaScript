493baafce9d88796466e20b6b60b5dda1b3cac4e
jugunbehera <117174558+jugunbehera@users.noreply.github.com>
2024-06-17 16:07:32 +0000

    Few More changes

--- 01_basics/04_DTnMemory.js
+++ 01_basics/04_DTnMemory.js
@@ -14,18 +14,38 @@
 let Heroes = ['Shaktiman','Aryaman','Krrish','Junior-G']
 // console.log(Heroes[0]+' '+ Heroes[3]);
 
-//Object
-// let MyObject = {
-//     Name : 'Jugun Behera',
-//     Mobile : '9036507261',
-// }
+Object
+let MyObject = {
+    Name : 'Jugun Behera',
+    Mobile : '9036507261',
+}
 // console.log(MyObject.Name+' '+MyObject.Mobile);
 
-//Function
-// const MyFunction = Function(
-//     console.log("This is my Function!!!")
-// )
+Function
+const MyFunction = Function(
+    2+2
+)
 
 //console.log(typeof Heroes);
 
 //typeof is a function used to find the data type.
+
+let Number=23;
+let Word = "23";
+let IsActive = true;
+let Money = null;
+let DreamCompany;
+let LoginId = Symbol('1234');
+console.log(typeof MyFunction)
+/*
+Interview Question : (TypeOf Function Return)
+Number = Number
+String = String
+Boolean = Boolean
+Undefined = Undefined
+Symbol = Symbol
+Object = Object
+Function = Function
+Null = object
+Array = Object
+*/
